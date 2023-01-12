14b629318d097cf65166d5488f669d0f495b4a05
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-12 21:05:13 +0700

    enhancement - clean up route api

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\RecipeController;
-use App\Http\Controllers\StoreController;
-use App\Http\Controllers\TopicController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,39 +14,31 @@ use Illuminate\Support\Facades\URL;
 */
 
 //!! PUBLIC ROUTE
-Route::post('register', [UserController::class, 'register']);
-Route::post('login', [UserController::class, 'login']);
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
 
-Route::get('recipe', [RecipeController::class, 'index']);
-Route::get('recipe/{id}', [RecipeController::class, 'show']);
+Route::get('recipe', 'RecipeController@index');
+Route::get('recipe/{id}', 'RecipeController@show');
 
-Route::get('store', [StoreController::class, 'index']);
-Route::get('store/{id}', [StoreController::class, 'show']);
+Route::get('store', 'StoreController@index');
+Route::get('store/{id}', 'StoreController@show');
 
-Route::get('product', [ProductController::class, 'index']);
-Route::get('product/{id}', [ProductController::class, 'show']);
+Route::get('product', 'ProductController@index');
+Route::get('product/{id}', 'ProductController@show');
 
-Route::get('topics', [TopicController::class, 'index']);
-Route::get('topics/{id}', [TopicController::class, 'show']);
+Route::get('topics', 'TopicController@index');
+Route::get('topics/{id}', 'TopicController@show');
 
 //!! PRIVATE ROUTE
-Route::middleware('auth:sanctum')->group(function(){
-    Route::post('logout', [UserController::class, 'logout']);
-    Route::get('upgrade/{id}', [UserController::class, 'upgradeToPremium']);
-    Route::resource('recipe', RecipeController::class)->except([
-        'index', 'show'
-    ]);
-    Route::resource('store', StoreController::class)->except([
-        'index', 'show'
-    ]);
-    Route::resource('product', ProductController::class)->except([
-        'index', 'show'
-    ]);
-    Route::resource('topics', TopicController::class)->except([
-        'index', 'show'
-    ]);
-});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', 'UserController@index');
+    Route::post('logout', 'UserController@logout');
+    Route::get('upgrade/{id}', 'PremiumController@update');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::resources([
+        'recipe' => 'RecipeController',
+        'store' => 'StoreController',
+        'product' => 'ProductController',
+        'topics' => 'TopicController'
+    ], ['except' => ['index', 'show']]);
 });
