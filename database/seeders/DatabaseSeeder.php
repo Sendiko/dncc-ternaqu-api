ecf83ab36713eba83045e7cae66664f33274df63
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-01-12 21:47:51 +0700

    enhancement - add seeder system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            RecipeSeeder::class,
+            ProductSeeder::class,
+            StoreSeeder::class,
+            TopicSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/StoreSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Store;
+use Illuminate\Database\Seeder;
+
+class StoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $stores = Store::factory(10)->make();
+
+        Store::insert($stores->toArray());
+    }
+}
